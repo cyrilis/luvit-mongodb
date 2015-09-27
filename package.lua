8570d250d13ddf48d75721dde5d4cb7d8ca057f9
Cyril Hou <houshoushuai@gmail.com>
2015-09-27 21:53:45 +0800

    [master] add extra info

--- package.lua
+++ package.lua
@@ -1,6 +1,11 @@
 return {
   name = "cyrilis/luvit-mongodb",
   version = "0.0.1",
+  homepage = "https://github.com/cyrilis/luvit-mongodb",
+  description = "Mongodb Driver in pure luvit.",
+  tags = {"luvit", "mongodb", "database"},
+  license = "MIT",
+  author = { name = "Cyril Hou" },
   files = {
     "**.lua",
   }
