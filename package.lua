fe83f3a2c9384961d02d0dcc479c866235d14811
Cyril Hou <houshoushuai@gmail.com>
2015-11-21 17:13:33 +0800

    [master] fix dependencies

--- package.lua
+++ package.lua
@@ -1,11 +1,17 @@
 return {
   name = "cyrilis/luvit-mongodb",
-  version = "0.0.5",
+  version = "0.0.6",
   homepage = "https://github.com/cyrilis/luvit-mongodb",
   description = "Mongodb Driver in pure luvit.",
   tags = {"luvit", "mongodb", "database"},
   license = "MIT",
   author = { name = "Cyril Hou" },
+  dependencies = {
+    "luvit/require",
+    "luvit/pretty-print",
+    "luvit/core",
+    "luvit/net"
+  },
   files = {
     "**.lua",
   }
