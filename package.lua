ee39166005127eacf39bb7205bce65275e02dde0
Cyril Hou <houshoushuai@gmail.com>
2017-01-18 17:38:52 +0800

    [master] Bump version number

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "cyrilis/luvit-mongodb",
-  version = "0.0.23",
+  version = "0.0.24",
   homepage = "https://github.com/cyrilis/luvit-mongodb",
   description = "Mongodb Driver in pure luvit.",
   tags = {"luvit", "mongodb", "database"},
